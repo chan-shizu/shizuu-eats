24e59690e6af72fec6053396c630b89af47ce90d
KoichiroShizuya <bubbler018@gmail.com>
2024-09-15 09:51:56 +0900

    deploy検証

--- api/deploy/after_install.sh
+++ api/deploy/after_install.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 
-echo "#################### 1 #########################"
-npm i
+cd /home/ec2-user/
+sudo chmod 777 package.json >& /home/ec2-user/stderr1.log
 
-echo "#################### 2 #########################"
-npm run build
+# who > user.txt
+# su ec2-user
+sudo yum install -y nodejs npm  >& /home/ec2-user/stderr2.log
+npm i >& /home/ec2-user/stderr3.log
 
-echo "#################### 3 #########################"
-npm run start
+npm run build >& /home/ec2-user/stderr4.log
+
+PID=$(sudo lsof -i :3000 -t)
+echo "$PID" > PID.txt
+if [ -n "$PID" ]; then
+    touch "has_PID.txt"
+    sudo kill "$PID" >& /home/ec2-user/stderr5.log
+else
+    touch "not_has_PID.txt"
+    echo "ポート 3000 でリッスンしているプロセスはありません。" >& /home/ec2-user/stderr6.log
+fi
+
+npm start &  >& /home/ec2-user/stderr7.log

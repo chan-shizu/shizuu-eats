9cad7b02aea04f0b0d14e1fce0ca7f2668d8caff
KoichiroShizuya <bubbler018@gmail.com>
2024-09-24 19:30:33 +0900

    add: Next Anaytics追加

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "react-hot-toast";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,6 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Analytics />
       <Toaster />
       <body className={inter.className}>{children}</body>
     </html>
